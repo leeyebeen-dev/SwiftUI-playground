b55d36ebbe97e4b386d95c6ff79ad89d3e4b2e11
Yecong <84004751+leeyebeen-dev@users.noreply.github.com>
2023-12-19 08:57:58 +0900

    Initial Commit

---
+++ SwiftUI-playground/SwiftUI_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_playgroundApp.swift
+//  SwiftUI-playground
+//
+//  Created by 이예빈 on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
