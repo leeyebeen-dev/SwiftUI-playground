992f91eeb367be29be5123fade615c17101b14e2
leeyebeen-dev <lyb0201190@gmail.com>
2024-01-09 19:39:06 +0900

    ❤️: ActionSheet 사용해보기

--- SwiftUI-playground/ContentView.swift
+++ SwiftUI-playground/ContentView.swift
@@ -8,16 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showingSheet = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Action Sheet 사용하기"){
+            showingSheet = true
         }
-        .padding()
+        .confirmationDialog("타이틀", isPresented: $showingSheet){
+            Button("제거", role: .destructive) {}
+            Button("취소", role: .cancel) {}
+        }
     }
 }
+
+// MARK: - ActionSheet의 과거
+// ActionSheet(title: Text, message: Text?, buttons: [ActionSheet.Button])
+
+// MARK: - ActionSheet의 현재
+// View 프로토콜의 modifier인 confirmationDialog(...)을 사용해서 ActionSheet을 대체할 수 있음
 
 #Preview {
     ContentView()
