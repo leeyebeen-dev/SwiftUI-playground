c04f3b2d2c7286dddd27f7d31f6f98c108f20fdb
leeyebeen-dev <lyb0201190@gmail.com>
2024-01-09 19:32:45 +0900

    🩷: SwiftUI 초급 강의 - Alert 띄우기

--- SwiftUI-playground/ContentView.swift
+++ SwiftUI-playground/ContentView.swift
@@ -8,16 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showing = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Alert 띄우기") {
+            showing = true
         }
-        .padding()
+        .alert("메시지 내용 입력", isPresented: $showing){
+            Button("OK", role: .cancel){}
+        }
+        
     }
 }
+
+// MARK: - Alert 창의 과거
+// iOS 이전 버전 지원을 위해서는 Alert(title: Text, message: Text?, dismissButton: Alert.Button?) 를 사용하여 구현함
+
+// MARK: - Alert 창의 현재
+// iOS 15부터는 alert modifier로 Alert이 대체됨
+
 
 #Preview {
     ContentView()
