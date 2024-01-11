25f375ab230b198ec14b5ab70172e9ed823b8edf
leeyebeen-dev <lyb0201190@gmail.com>
2024-01-11 10:58:15 +0900

    💚: TextField,SecureField, TextEditor 사용해보기

--- SwiftUI-playground/ContentView.swift
+++ SwiftUI-playground/ContentView.swift
@@ -8,16 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var text: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // TextEditor 사용해보기
+            TextEditor(text: $text)
+                .cornerRadius(10)
+                .padding()
+                .background(Color.blue)
         }
-        .padding()
     }
 }
+
+// MARK: - TextField로부터 Text값 불러오기
+// TextField("placeholder", text: $변수명)
+
+/*
+ @State var name: String = ""
+ 
+ var body: some View {
+     VStack {
+         TextField("Enter your name", text: $name)
+             .padding()
+             .background(Color.blue)
+             .foregroundColor(Color.white)
+         
+         Divider()
+         
+         Text("Hi, \(name)!")
+     }
+     .padding()
+ }
+ */
+
+// MARK: - TextField 스타일 변경하기
+// .textFieldStyle(.roundedBorder)
+
+// MARK: - Secure TextField 사용하기
+/*
+ SecureField("비밀번호 8자리를 입력하세요.", text: $password)
+     .padding()
+     .background(Color.blue)
+     .textFieldStyle(.roundedBorder)
+ */
+
 
 #Preview {
     ContentView()
