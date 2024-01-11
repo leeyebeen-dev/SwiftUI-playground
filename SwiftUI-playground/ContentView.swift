af63e4a914c252026ee14eb72972fe888f06b88a
leeyebeen-dev <lyb0201190@gmail.com>
2024-01-11 13:02:55 +0900

    💙: 여러 picker 사용해보기

--- SwiftUI-playground/ContentView.swift
+++ SwiftUI-playground/ContentView.swift
@@ -7,17 +7,67 @@
 
 import SwiftUI
 
+// MARK: - ColorPicker 사용해보기
+
 struct ContentView: View {
+    @State var firstColor = Color.yellow
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ColorPicker("색상을 골라요!", selection: $firstColor)
+                .padding()
+                .background(firstColor)
+                .cornerRadius(10)
+                .padding()
         }
-        .padding()
     }
 }
+
+// MARK: - Picker 사용해보기
+
+/*
+ struct ContentView: View {
+     var colorName = ["Blue", "SkyBlue", "Alice", "CobaltBlue", "ClassicBlue"]
+     @State var selectedColor = ""
+     
+     var body: some View {
+         VStack {
+             Picker("Choose a bluuu color", selection: $selectedColor){
+                 ForEach(colorName, id: \.self) {
+                     Text($0)
+                 }
+             }
+             .pickerStyle(.inline)
+             .background(Color.yellow)
+             .cornerRadius(10)
+             .padding()
+             
+             Text("you selected \(selectedColor)!")
+         }
+     }
+ }
+ */
+
+// MARK: - DatePicker 사용해보기
+/*
+ struct ContentView: View {
+     @State var date = Date()
+     
+     var body: some View {
+         VStack {
+             DatePicker(
+                 "StartDate",
+                 selection: $date,
+                 displayedComponents: [.date]
+             )
+             .padding()
+             .background(Color.yellow)
+             .cornerRadius(10)
+             .padding(/*@START_MENU_TOKEN@*/EdgeInsets()/*@END_MENU_TOKEN@*/)
+         }
+     }
+ }
+ */
 
 #Preview {
     ContentView()
